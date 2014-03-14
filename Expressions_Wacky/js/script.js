2cb242fd896c885c86939aac4b3f078fac539764
Laurie Stirling <lstirling@fulsail.edu>
2014-03-13 22:22:54 -0500

    added equation and comments

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -9,3 +9,6 @@ document.querySelector("body").style.backgroundColor= "#00FF00";
 var birds = 6; //how many birds found in the yard
 var toes = 3; //how many toes are on each bird foot
 var feet = 2; //how many feet the birds have
+
+var feetTotal = birds * toes * feet + " bird toes found in the yard" //the total of birds times the total of toes on each foot equals the number of toes in the yard
+console.log(feetTotal);
