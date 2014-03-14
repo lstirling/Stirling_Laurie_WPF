3fc7601476f4867b543413d5c2d1c0e8af670217
Laurie Stirling <lstirling@fulsail.edu>
2014-03-13 21:17:42 -0500

    entered result dialogue box and comments

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -8,8 +8,9 @@ document.querySelector("body").style.backgroundColor= "#00FF00";
 
 var dogWeight = "1"; //dog's weight in lbs
 var benedryl = 1; //benedryl dose in mg
-var doseage = dogWeight * benedryl //dogs doseage of benedryl is 1mg per lb of body weight
+var doseage = dogWeight * benedryl //dogs doseage of benedryl 1 mg per lb of body weight
 
 var dogWeight = prompt("Enter your dog's weight here:"); //dog weight dialogue box for calculations
-var doseage = "Your dog's benedryl doseage is " + dogWeight * benedryl + " mg";
+var doseage = "Your dog's benedryl doseage is " + dogWeight * benedryl + " mg"; //user entered dogs weight times 1mg equals benedryl doseage
+alert(doseage); //result dialogue box
 
