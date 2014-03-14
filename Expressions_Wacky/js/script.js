9dc2714b7f9cf9d822d0edd7ff9507b6650c195b
Laurie Stirling <lstirling@fulsail.edu>
2014-03-13 22:18:58 -0500

    added Wacky Expressions title as well as bird variable

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -3,3 +3,7 @@
 //this changes the css of your body tag to be green
 document.querySelector("body").style.backgroundColor= "#00FF00";
 //Laurie Stirling, Expressions_Wacky, 3-13-14
+
+//Based on the number of birds in the yard, how many toes?
+
+var birds = 6;
