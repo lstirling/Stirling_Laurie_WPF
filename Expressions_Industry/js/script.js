39b9dcba57a30fdd2636f476550f269fae4d6cc1
Laurie Stirling <lstirling@fulsail.edu>
2014-03-13 21:06:59 -0500

    entered dogWeight variable

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -3,3 +3,7 @@
 //this changes the css of your body tag to be green
 document.querySelector("body").style.backgroundColor= "#00FF00";
 //Laurie Stirling, Expressions_Industry, 3-13-14
+
+//How to calculate dog's Benedryl doseage by weight
+
+var dogWeight = "1"; 
