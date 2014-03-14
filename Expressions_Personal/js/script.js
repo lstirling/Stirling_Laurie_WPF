07e0a31e03b3f27e3ca76d9a53262567fa37e5a1
Laurie Stirling <lstirling@fulsail.edu>
2014-03-13 21:18:32 -0500

    Entered Expressions description

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -3,3 +3,6 @@
 //this changes the css of your body tag to be green
 document.querySelector("body").style.backgroundColor= "#00FF00";
 //Laurie Stirling, Expressions_Personal, 3-13-14
+
+//How many cans of food do my cats eat per day
+
