d6cf0015e3762f27e2dd02035dc465cc24013456
Laurie Stirling <lstirling@fulsail.edu>
2014-03-20 20:28:13 -0500

    finished Ternary

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -12,4 +12,4 @@ var game; //game that can be played
 
 //if Andrew is over 13 he can play Diablo, otherwise he can play Hearthstone.
 
-game = (andrewAge >13)
+game = (andrewAge > 13) ? "Andrew can play Diablo!" : "Andrew can play Hearthstone!";
