eea12198eb0eb5eabe4213acb8c276d2ae2e408e
Laurie Stirling <lstirling@fulsail.edu>
2014-03-20 20:15:31 -0500

    Added variable Andrew and comment

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -5,3 +5,6 @@ document.querySelector("body").style.backgroundColor= "#00FF00";
 
 //Laurie Stirling, Conditionals_Personal, 3-20-14
 
+//What game can my son play?
+
+var Andrew = 10 // My son Andrew is 10 years old
