d258e9e09bb01f063feddfd30ae792c4737cb4e5
Laurie Stirling <lstirling@fulsail.edu>
2014-03-20 22:16:31 -0500

    added if logical operator

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -8,3 +8,7 @@ document.querySelector("body").style.backgroundColor= "#00FF00";
 var taxReturn = 5000; // amount of tax return expected
 var vacaPrice = 3500; // expected amount for vacation
 var budget = 4000 // maximum amount to spend on vacation
+
+if(vacaPrice < budget && taxReturn > 5000){
+	
+}
