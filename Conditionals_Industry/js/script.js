5b29cc31279201cc64fb01e2be2d5ea843eec704
Laurie Stirling <lstirling@fulsail.edu>
2014-03-20 21:55:19 -0500

    added semi colons and curly brace

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -5,8 +5,9 @@ document.querySelector("body").style.backgroundColor= "#00FF00";
 
 //Laurie Stirling, Conditionals_Industry, 3-20-14
 
-var temp = 35 //current temparature outside is 35 degrees
-var lowTemp = 32 // minumum temperature outside to travel to work
+var temp = 35; //current temparature outside is 35 degrees
+var lowTemp = 32; // minumum temperature outside to travel to work
 
 if(temp > lowTemp){ // if the temperature is higher than 35 degrees you can travel to work.
-	console.log ("You can safely travel to work today!");
+	console.log("You can safely travel to work today!");
+}
