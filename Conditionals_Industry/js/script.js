3fc1440ca6a558311be46e2dd51010547108ecb8
Laurie Stirling <lstirling@fulsail.edu>
2014-03-20 22:04:53 -0500

    fixed else statement

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -8,10 +8,9 @@ document.querySelector("body").style.backgroundColor= "#00FF00";
 var temp = 35; //current temparature outside is 35 degrees
 var lowTemp = 32; // minumum temperature outside to travel to work
 
-if(temp > lowTemp){ // if the temperature is higher than 35 degrees you can travel to work.
+if(temp > lowTemp){ // if the temperature is higher than 32 degrees you can travel to work.
 	console.log("You can safely travel to work today!"); //validation of code	
-}
 
-}else{ //if temp is below 32 degrees than weather conditions are not safe for travel
-	console.log("Conditions are not safe for travel due to temperature.");
-}	
+}else{ //if temperature is below 32 degrees conditions are not safe for travel.
+	console.log("It is not safe to travel today due to weather conditions");
+}
