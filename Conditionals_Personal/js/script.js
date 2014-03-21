ebba996275839f19e890140cdb4b718401f1f406
Laurie Stirling <lstirling@fulsail.edu>
2014-03-20 20:26:51 -0500

    fixed Andrew variable and started Ternary

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -7,8 +7,9 @@ document.querySelector("body").style.backgroundColor= "#00FF00";
 
 //What game can my son play?
 
-var Andrew = 10 // My son Andrew is 10 years old
+var andrewAge = 10 // My son Andrew is 10 years old
 var game; //game that can be played
 
 //if Andrew is over 13 he can play Diablo, otherwise he can play Hearthstone.
 
+game = (andrewAge >13)
