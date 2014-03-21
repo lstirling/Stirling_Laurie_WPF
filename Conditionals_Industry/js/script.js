209b0927b27d82e0c991a1bce126485d8d942d30
Laurie Stirling <lstirling@fulsail.edu>
2014-03-20 22:05:30 -0500

    validated code

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -5,12 +5,12 @@ document.querySelector("body").style.backgroundColor= "#00FF00";
 
 //Laurie Stirling, Conditionals_Industry, 3-20-14
 
-var temp = 35; //current temparature outside is 35 degrees
+var temp = 29; //current temparature outside is 35 degrees
 var lowTemp = 32; // minumum temperature outside to travel to work
 
 if(temp > lowTemp){ // if the temperature is higher than 32 degrees you can travel to work.
 	console.log("You can safely travel to work today!"); //validation of code	
 
 }else{ //if temperature is below 32 degrees conditions are not safe for travel.
-	console.log("It is not safe to travel today due to weather conditions");
-}
+	console.log("It is not safe to travel today due to weather conditions.");
+} //validated code
