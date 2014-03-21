f1d6ea09ad3b3960516afb47892775ee1c92aae9
Laurie Stirling <lstirling@fulsail.edu>
2014-03-20 22:09:33 -0500

    added taxReturn Variable

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -2,3 +2,7 @@
 
 //this changes the css of your body tag to be green
 document.querySelector("body").style.backgroundColor= "#00FF00";
+
+//Laurie Stirling, Conditionals_Wacky, 3-20-14
+
+var taxReturn = 5000;
