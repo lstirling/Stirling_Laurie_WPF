3f416b0eb9af8a76b007e1a65a266bd534506621
Laurie Stirling <lstirling@fulsail.edu>
2014-03-27 22:59:06 -0500

    changed file names and added html title

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -2,18 +2,3 @@
 
 //this changes the css of your body tag to be green
 document.querySelector("body").style.backgroundColor= "#00FF00";
-
-//Laurie Stirling, Functions_Industry, 3-27-14
-
-var kidWeight = 35; //Kids weight in pounds
-var minWeight = 40; //Minimum weight to ride in front seat of car.
-var airbagControl = 40 // max weight to ride in front seat with airbag turned off.
-
-if(kidWeight > minWeight){ //if kids weight is less than 50 lbs he can sit up front
-
-}else  if(kidWeight < minWeight) // can ride up front if under 40 lbs so airbag does not come on.
-	
-while  (kidWeight < 40){
-	console.log( kidWeight +"lbs You can ride up front!");
-	kidWeight++;
-}
