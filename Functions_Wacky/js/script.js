c9e9be31ac08c3877bd8c5e7728da7d13ac07710
Laurie Stirling <lstirling@fulsail.edu>
2014-03-27 23:33:35 -0500

    added volume variables and named assignment

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -2,3 +2,9 @@
 
 //this changes the css of your body tag to be green
 document.querySelector("body").style.backgroundColor= "#00FF00";
+
+//Laurie Stirling, Functions_Wacky, 3-27-14
+
+var volume = 15;
+var maxVolume = 50;
+
