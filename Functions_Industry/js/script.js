e0fde403296e323484964de9fd395448339e7f12
Laurie Stirling <lstirling@fulsail.edu>
2014-03-27 22:37:15 -0500

    added variables

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -6,4 +6,8 @@ document.querySelector("body").style.backgroundColor= "#00FF00";
 //Laurie Stirling, Functions_Industry, 3-27-14
 
 var kidWeight = 35; //Kids weight in pounds
+var minWeight = 40; //Minimum weight to ride in front seat of car.
+var airbagControl = 40 // max weight to ride in front seat with airbag turned off.
 
+if(kidWeight < minWeight) //if kids weight is less than 50 lbs he can sit up front
+	console.log("You can ride in front seat!");
