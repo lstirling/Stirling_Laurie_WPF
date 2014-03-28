a7023813542a15557685ae0a7806beed6036ebac
Laurie Stirling <lstirling@fulsail.edu>
2014-03-27 23:08:06 -0500

    added ternary

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -5,3 +5,10 @@ document.querySelector("body").style.backgroundColor= "#00FF00";
 
 //Laurie Stirling, Functions_Industry, 3-27-14
 
+var heartRate = 125;
+var lowRate = 100;
+var highRate = 200;
+
+if(heartRate > 100){
+	console.log("Heart rate is stable.")
+}
