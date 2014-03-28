dd0943d6e1a49bfd498e08c2ed7e1451905cfb7e
Laurie Stirling <lstirling@fulsail.edu>
2014-03-27 23:18:47 -0500

    added loop

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -5,10 +5,18 @@ document.querySelector("body").style.backgroundColor= "#00FF00";
 
 //Laurie Stirling, Functions_Industry, 3-27-14
 
-var heartRate = 125;
-var lowRate = 100;
-var highRate = 200;
+var heartRate = 125; //patients heart rate
+var lowRate = 100; //heart rate that is too low
+var highRate = 200; //heart rate that is too fast
+var normalRate;
 
-if(heartRate > 100){
-	console.log("Heart rate is stable.")
+if(heartRate >100){ //conditional
+	normalRate = "Heart rate is normal."
+	
+}else heartRate = "Heart rate is too high!"
+console.log(normalRate);
+
+while (heartRate <200){
+	console.log(heartRate + " is a normal heart rate.")
+	heartRate++;
 }
