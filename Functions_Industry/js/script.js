9ff840d213e9f64e872929104474085a702ff21f
Laurie Stirling <lstirling@fulsail.edu>
2014-03-27 22:33:46 -0500

    added kidWeight Var

---
+++ Functions_Industry/js/script.js
@@ -0,0 +1,9 @@
+// JavaScript
+
+//this changes the css of your body tag to be green
+document.querySelector("body").style.backgroundColor= "#00FF00";
+
+//Laurie Stirling, Functions_Industry, 3-27-14
+
+var kidWeight = 35; //Kids weight in pounds
+
